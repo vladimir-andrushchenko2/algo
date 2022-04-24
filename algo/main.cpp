b5274ea65cff5ce8750f7580b0bfd415aeb163e9
Vladimir Andrushchenko <andrushchenko.v@icloud.com>
2022-04-24 23:16:36 +0300

    Initial Commit

---
+++ algo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  algo
+//
+//  Created by Vladimir Andrushchenko on 24.04.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
