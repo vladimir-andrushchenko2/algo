47ecbd8b7b34e565a040edd0dc225c4f91a5d696
Vladimir Andrushchenko <andrushchenko.v@icloud.com>
2022-04-28 18:31:44 +0300

    OK

--- longest_increasing_path/main.cpp
+++ longest_increasing_path/main.cpp
@@ -5,49 +5,53 @@
 
 using namespace std;
 
+using Matrix = vector<vector<size_t>>;
+
+size_t DFS(int row, int col, const Matrix& matrixs, Matrix& dp) {
+    if (dp[row][col] > 0) {
+        return dp[row][col];
+    }
+    
+    size_t max_route = 0;
+    
+    // check left
+    if (col - 1 >= 0 && matrixs[row][col] < matrixs[row][col - 1]) {
+        max_route = DFS(row, col - 1, matrixs, dp) + 1;
+    }
+    
+    // check right
+    if (col + 1 < matrixs.front().size() && matrixs[row][col] < matrixs[row][col + 1]) {
+        max_route = std::max(DFS(row, col + 1, matrixs, dp) + 1, max_route);
+    }
+    
+    // check up
+    if (row - 1 >= 0 && matrixs[row][col] < matrixs[row - 1][col]) {
+        max_route = std::max(DFS(row - 1, col, matrixs, dp) + 1, max_route);
+    }
+    
+    // check down
+    if (row + 1 < matrixs.size() && matrixs[row][col] < matrixs[row + 1][col]) {
+        max_route = std::max(DFS(row + 1, col, matrixs, dp) + 1, max_route);
+    }
+    
+    // if couldn't move anywhere len is 1
+    size_t output = max_route > 0 ? max_route : 1;
+    
+    dp[row][col] = output;
+    
+    return output;
+}
+
 size_t getLongestIncreasingPath(size_t n, size_t m, const vector<vector<size_t>>& matrixs) {
     vector<vector<size_t>> dp(n, vector<size_t>(m));
     
     // accepts starting coordinates
-    std::function<size_t(int, int)> dfs;
-    
-    dfs = [&](int row, int col) {
-        if (dp[row][col] > 0) {
-            return dp[row][col];
-        }
-        
-        size_t max_route = 0;
-        
-        // check left
-        if (col - 1 >= 0 && matrixs[row][col] < matrixs[row][col - 1]) {
-            max_route = dfs(row, col - 1) + 1;
-        }
-        
-        // check right
-        if (col + 1 < m && matrixs[row][col] < matrixs[row][col + 1]) {
-            max_route = std::max(dfs(row, col + 1) + 1, max_route);
-        }
-        
-        // check up
-        if (row - 1 >= 0 && matrixs[row][col] < matrixs[row - 1][col]) {
-            max_route = std::max(dfs(row - 1, col) + 1, max_route);
-        }
-        
-        // check down
-        if (row + 1 < n && matrixs[row][col] < matrixs[row + 1][col]) {
-            max_route = std::max(dfs(row + 1, col) + 1, max_route);
-        }
-        
-        // if couldn't move anywhere len is 1
-        return max_route > 0 ? max_route : 1;
-    };
-    
     size_t total_max = 1;
     
     for (int row = 0; row < n; ++row) {
         for (int col = 0; col < m; ++col) {
             if (dp[row][col] == 0) {
-                size_t route_len = dfs(row, col);
+                size_t route_len = DFS(row, col, matrixs, dp);
                 
                 total_max = total_max < route_len ? route_len : total_max;
             }
